d8583fac654b0ffbb5f5e25fb7a6f7118c8efeb0
“[Piyush <118821247+Piyush0053@users.noreply.github.com>
2025-06-24 23:47:07 +0530

    production

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,13 +9,28 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig); // Initialize app globally
+let firebaseApp;
+let analytics;
+
+// Initialize Firebase only once
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+  
+  // Initialize Analytics only in client-side and if supported
+  if (typeof window !== 'undefined') {
+    isSupported().then(yes => yes && (analytics = getAnalytics(firebaseApp)));
+  }
+}
+
 const auth = getAuth();
 const db = getFirestore();
 const googleProvider = new GoogleAuthProvider();
+
+export { analytics };
 
 export { auth, db, googleProvider };
